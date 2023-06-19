3c5aa24462b9f08139d069dacff112a6ad205213
luanafront <luana.lopes.rodrigues1@gmail.com>
2023-06-19 15:07:27 -0300

    just put the image and have the option to open camera

--- index.js
+++ index.js
@@ -1,62 +1,17 @@
 let imageUpload = document.getElementById('imageUpload');
-let imageContainer = document.getElementById('imageContainer');
-let cameraButton = document.getElementById('cameraButton');
-let photoCanvas = document.getElementById('photoCanvas');
-let photoContext = photoCanvas.getContext('2d');
-let videoStream;
 
 imageUpload.addEventListener('change', function(event) {
   let file = event.target.files[0];
+
   let reader = new FileReader();
 
   reader.onload = function(event) {
-    let image = new Image();
+    let image = document.createElement('img');
     image.src = event.target.result;
+
+    let imageContainer = document.getElementById('imageContainer');
     imageContainer.appendChild(image);
   };
 
   reader.readAsDataURL(file);
-});
-
-cameraButton.addEventListener('click', function() {
-  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({ video: true })
-      .then(function(stream) {
-        videoStream = stream;
-        let video = document.createElement('video');
-        video.srcObject = videoStream;
-        video.play();
-
-        let captureFrame = function() {
-          photoCanvas.width = video.videoWidth;
-          photoCanvas.height = video.videoHeight;
-          photoContext.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
-
-          let imageDataURL = photoCanvas.toDataURL('image/png');
-          console.log(imageDataURL);
-        };
-
-        let exitCamera = function() {
-          stopCamera();
-          cameraButton.removeEventListener('click', captureFrame);
-          cameraButton.removeEventListener('click', exitCamera);
-        };
-
-        let stopCamera = function() {
-          video.pause();
-          video.srcObject = null;
-          videoStream.getTracks().forEach(function(track) {
-            track.stop();
-          });
-        };
-
-        cameraButton.addEventListener('click', captureFrame);
-        cameraButton.addEventListener('click', exitCamera);
-      })
-      .catch(function(error) {
-        console.error('Ocorreu um erro ao acessar a câmera:', error);
-      });
-  } else {
-    console.error('A captura de mídia não é suportada pelo seu navegador.');
-  }
 });
